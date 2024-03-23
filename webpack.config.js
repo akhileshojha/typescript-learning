44b77915e5ae995b5a8c4565d826049b445780ed
Akhilesh Ojha <33337618+akhileshojha@users.noreply.github.com>
2024-03-24 00:06:39 +0530

    Typescript and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+
+};
